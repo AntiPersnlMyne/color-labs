d85dcbd456a4e45ae76c3ffdabe7548eea18a94f
Gian-Mateo <64568308+AntiPersnlMyne@users.noreply.github.com>
2023-10-31 12:36:23 -0400

    Update Proj5.m

--- Project 5/Proj5.m
+++ Project 5/Proj5.m
@@ -1,2 +1,9 @@
-Proj 5
+%% Credits
+% Team #: 1 |
+% Authors: Cooper White & Gian-Mateo Tifone |
+% Date: 10/23/2023
 
+%% Step 1 - Initialization
+cie = loadCIEdata;  
+load('loadPatchData.mat') % Read in patches
+CCPNG = imread('chart.jpg'); % Read in Color checker PNG
